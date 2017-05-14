d97b234f934f0d55935295d6078934b1898a4198
An rumboutique <a.skurlatov@gmail.com>
2017-05-15 00:46:18 +0300

    v 0.2.6.8 Sidenav collapse button fixed

--- src/components/Dash.vue
+++ src/components/Dash.vue
@@ -14,7 +14,7 @@
       <!-- Header Navbar -->
       <nav class="navbar navbar-static-top" role="navigation">
         <!-- Sidebar toggle button-->
-        <a href="javascript:;"  data-toggle="modal" data-target="#formModal" role="button">
+        <a href="javascript:;" class="sidebar-toggle" data-toggle="offcanvas" role="button">
           <span class="sr-only">Toggle navigation</span>
         </a>
         <!-- Navbar Right Menu -->

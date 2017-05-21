2dfc2320365f8293370284e56cba66d142188c77
An rumboutique <a.skurlatov@gmail.com>
2017-05-21 03:08:27 +0300

    To nav buttons changed

--- src/components/Dash.vue
+++ src/components/Dash.vue
@@ -14,14 +14,14 @@
       <!-- Header Navbar -->
       <nav class="navbar navbar-static-top" role="navigation">
         <!-- Sidebar toggle button-->
-        <a href="javascript:;" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+        <a href="javascript:;" class="sidebar-toggle fade-hover" data-toggle="offcanvas" role="button">
           <span class="sr-only">Toggle navigation</span>
         </a>
         <!-- Navbar Right Menu -->
         <div class="navbar-custom-menu">
           <ul class="nav navbar-nav">
             <li>
-              <a href="javascript:;" @click="showSendModal = true">
+              <a href="javascript:;" @click="showSendModal = true" class="fade-hover">
                 Send
                 <i class="fa fa-share-square-o"></i>
               </a>
@@ -42,7 +42,7 @@
             </li> -->
             <!-- Messages-->
             <li class="dropdown messages-menu">
-              <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown">
+              <a href="javascript:;" class="dropdown-toggle fade-hover" data-toggle="dropdown">
                 <i class="fa fa-envelope-o"></i>
                 <span class="label label-success">{{ userInfo.messages | count }}</span>
               </a>
@@ -77,7 +77,7 @@
 
             <!-- Notifications Menu -->
             <li class="dropdown notifications-menu">
-              <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown">
+              <a href="javascript:;" class="dropdown-toggle fade-hover" data-toggle="dropdown">
                 <i class="fa fa-bell-o"></i>
                 <span class="label label-warning">{{ userInfo.notifications | count }}</span>
               </a>
@@ -134,30 +134,24 @@
             </li> -->
 
             <!-- User Account Menu -->
-            <li class="dropdown user user-menu">
-              <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown">
-                <div v-if="person">
+            <li v-if="person" class="dropdown user user-menu">
+              <a href="javascript:;" class="dropdown-toggle fade-hover" data-toggle="dropdown">
+                <div>
                   <img v-bind:src="demo.avatar" class="user-image" alt="User Image">
                   <span class="hidden-xs">{{ demo.displayName }}</span>
-                </div>
-                <div v-else>
-                  <i class="fa fa-user"></i> Anonim
                 </div>
               </a>
               <ul class="dropdown-menu shadow-3">
                 <!-- User image -->
-                <li v-if="person" class="user-header">
+                <li class="user-header">
                   <img v-bind:src="demo.avatar" class="img-circle" alt="User Image">
                   <p>{{ demo.displayName }}</p>
                 </li>
                 <li class="user-footer">
                   <div class="pull-left">
-                    <a v-if="person" @click="" href="javascript:;" class="btn btn-default btn-flat">
+                    <a @click="" href="javascript:;" class="btn btn-default btn-flat">
                       <i class="fa fa-user" aria-hidden="true"></i> Person
                     </a>
-                    <router-link to="/persons/new" v-else class="btn btn-default btn-flat">
-                      <i class="fa fa-user-plus" aria-hidden="true"></i> Add person
-                    </router-link>
                   </div>
                   <div class="pull-right">
                     <a @click="logout" href="javascript:;" class="btn btn-default btn-flat">
@@ -166,7 +160,16 @@
                   </div>
                 </li>
               </ul>
-              <!-- <a @click="logout" href="javascript:;"><i class="fa fa-sign-out" aria-hidden="true"></i>Logout</a> -->
+            </li>
+            <li v-else>
+              <router-link to="/persons/new" class="fade-hover">
+                <i class="fa fa-user-plus" aria-hidden="true"></i> Add person
+              </router-link>
+            </li>
+            <li v-if="!person">
+              <a @click="logout" href="javascript:;" class="fade-hover">
+                Sign out <i class="fa fa-sign-out" aria-hidden="true"></i>
+              </a>
             </li>
           </ul>
         </div>
